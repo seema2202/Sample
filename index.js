d8da26c28d178b25f4c3e05a0551cce27cc4937d
seema2202 <spsimmi@gmail.com>
2018-05-16 11:56:39 +0530

    Update index.js
    using single quotes for 'telephone'

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ restService.post('/v2/webhook',(req,res)=>{
     var mobile = req.body.queryResult.parameters.mobile;
     //var telephone= "'telephone'";
   
-    response = "Your "+planType+" plan purchase payment details are saved in our system. Please make a note, your mobile number <say-as interpret-as="telephone" format="39">09949337272 </say-as> will be used for future communications. If you wish to know anything more, please let me know.";
+    response = "Your "+planType+" plan purchase payment details are saved in our system. Please make a note, your mobile number <say-as interpret-as='telephone' format="39">09949337272</say-as> will be used for future communications. If you wish to know anything more, please let me know.";
     console.log(response)  
   
   }else if(action === 'input.cancel'){
